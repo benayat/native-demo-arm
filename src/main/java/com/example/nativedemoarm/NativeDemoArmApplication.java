77b1ebcd4b3f95cf67d2fd09a17711e0ae4c5380
benaya Trabelsi <benaya7@gmail.com>
2023-01-08 22:59:49 +0200

    .

---
+++ src/main/java/com/example/nativedemoarm/NativeDemoArmApplication.java
@@ -0,0 +1,13 @@
+package com.example.nativedemoarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeDemoArmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NativeDemoArmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nativedemoarm/NativeDemoArmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nativedemoarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeDemoArmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
